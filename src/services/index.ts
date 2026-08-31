5463a323d73bf83fafcd76d2a1c8908c4f687086
bruno <brunosilval1415@gmail.com>
2026-08-31 10:19:36 -0300

    feat: implementa método .calcularTotal no index.html

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,24 +1,53 @@
 import { Produto } from "../entities/produto.js";
 import { ItemPedido } from "../entities/itemPedido.js";
 import { ItemPedidoPromocional } from "../entities/itemPedidoPromocional.js";
+import { Pedido } from "../entities/pedido.js";
+import { Cliente } from "../entities/cliente.js"
 
+//==============================================
+// HU01 criar pedido e adicionar produtos
+//==============================================
 try {
-    const p1 = new Produto(1, "Notebook", 4000.00);
-    const p2 = new Produto(2, "Mouse", 100.00);
-    const itemRegular = new ItemPedido(p1, 2);
-//7
-    console.log(itemRegular.toString());
-// Criando item promocional omitindo o desconto opcional (assume 10%)
-    const itemPromo1 = new ItemPedidoPromocional(p2, 2);
-    console.log(itemPromo1.toString());
-// Criando item promocional informando desconto de 20%
-    const itemPromo2 = new ItemPedidoPromocional(p2, 5, 0.20);
-    console.log(itemPromo2.toString());
-} catch (error: unknown) { //8
-//9
+    const cliente1 = new Cliente("João")
+    const pedido1 = new Pedido(cliente1)
+
+    const p1 = new Produto(1, "Café", 5.00)
+    const p2 = new Produto(1, "Bolo", 8.00)
+    const p3 = new Produto(1, "Suco", 6.00)
+
+    const item1 = new ItemPedido(p1, 1)
+    const item2 = new ItemPedido(p2, 1)
+    const item3 =  new ItemPedido(p3, 1)
+
+    pedido1.adicionarItem(item1)
+    pedido1.adicionarItem(item2)
+    pedido1.adicionarItem(item3)
+
+    console.log(item1.toString())
+    console.log(item2.toString())
+    console.log(item3.toString())
+
+    console.log(`Total do pedido: ${pedido1.calcularTotal().toFixed(2)}`)
+} catch (error: unknown) {
     if (error instanceof Error) {
-        console.error(`Erro validado: ${error.message}`);
+        console.log(`Erro validado: ${error.message}`)
     } else {
-        console.error("Ocorreu um erro inesperado e desconhecido.");
+        console.error('Ocorreu um erro ineserado e desconhecido')
     }
 }
+
+//==============================================
+// HU02 remover produto do pedido
+//==============================================
+
+//==============================================
+// HU03 alterar quantidade de um produto
+//==============================================
+
+//==============================================
+// HU04 finalizar pedido
+//==============================================
+
+//==============================================
+// HU05 cancelarpedido
+//==============================================
